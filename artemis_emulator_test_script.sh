0ce9c757ff185022a9c4a5e3b2f2168575f16d97
Agostino <agostino.delucia@hotmail.it>
2021-03-19 22:49:47 +0100

    .

--- artemis_emulator_test_script.sh
+++ artemis_emulator_test_script.sh
@@ -14,6 +14,13 @@ cd ../broker || exit #path per la creazione del broker
 
 rm -rf mybroker
 
-artemis create mybroker --user licit --password licit --allow-anonymous 
+artemis create mybroker --user licit --password licit --allow-anonymous
+
+yes | cp -rf broker.xml mybroker/etc/broker.xml
 
 ./mybroker/bin/artemis.cmd run
+
+#--------------------------------
+#avviare l'emulatore da terminale
+#--------------------------------
+# mvn exec:java -Dexec.mainClass="Emulatorx"
